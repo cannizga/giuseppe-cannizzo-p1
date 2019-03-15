df126b2e053b6f512ab91b53b00f5708f3e19a14
unknown <giuseppeacannizzojr@gmail.com>
2019-03-14 23:54:02 -0500

    Third Commit

---
+++ Project1/js/scripts.js
@@ -0,0 +1,11 @@
+var greeting = 'Welcome!!';
+var user = "Giuseppe!!!";
+var message = greeting + " " + user;
+
+var price = 400;
+var studentdiscount = .15;
+var studentprice = price - (price * studentdiscount);
+var message2 = studentprice;
+
+var name = 'joe';
+var username = name;
