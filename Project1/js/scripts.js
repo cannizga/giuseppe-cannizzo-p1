ec5864eec7c65f3896addd8080e1d0437ae1481a
unknown <giuseppeacannizzojr@gmail.com>
2019-03-15 00:15:55 -0500

    Fifth Commit

--- Project1/js/scripts.js
+++ Project1/js/scripts.js
@@ -1,11 +1,8 @@
 var greeting = 'Welcome!!';
-var user = "Giuseppe!!!";
-var message = greeting + " " + user;
+var greeting2 = "To My Website";
+var message = greeting + " " + greeting2;
 
 var price = 400;
 var studentdiscount = .15;
 var studentprice = price - (price * studentdiscount);
 var message2 = studentprice;
-
-var name = 'joe';
-var username = name;
