10a5a628982632b5dda5b89b3aec108d43880bcd
unknown <giuseppeacannizzojr@gmail.com>
2019-03-19 22:44:21 -0500

    Complete Project 1

--- Project1/js/scripts.js
+++ Project1/js/scripts.js
@@ -1,8 +1,3 @@
 var greeting = 'Hope Your Having';
 var greeting2 = "A Good Day :)";
 var message = greeting + " " + greeting2;
-
-var price = 400;
-var studentdiscount = .15;
-var studentprice = price - (price * studentdiscount);
-var message2 = studentprice;
